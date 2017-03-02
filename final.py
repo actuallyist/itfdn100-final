7671848c0ffa61b4ec2173b1e424c542085042e1
unknown <sm.rambeau@gmail.com>
2017-03-02 10:52:51 -0800

    uploading psuedocode for final project

---
+++ final.py
@@ -0,0 +1,16 @@
+import sys
+import random
+
+# create menu: start new game of quit
+
+# create function for difficulty
+# difficulty determines player's health level range
+# health random in all cases
+
+# create function for room sequence (can use classes here maybe)?
+
+# create function for battle sequence
+# damage is random number between 1-10
+
+# create function for find potion
+# potion adds 10 health 
