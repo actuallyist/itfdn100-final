3d6f45397c17f63e298b0e64efc35864e9c78d5f
unknown <sm.rambeau@gmail.com>
2017-03-02 11:58:07 -0800

    added menu and random number generator

--- final.py
+++ final.py
@@ -1,7 +1,21 @@
 import sys
-import random
+from random import randint
+from sortedcontainers import SortedDict
 
 # create menu: start new game of quit
+def start_menu():
+	print("Welcome to the game!")
+	print("Press 1 to start a new game")
+	print("Press 2 to quit")
+
+# display menu
+start_menu()
+
+# create random number generator
+def random_number(num1, num2):
+	rand_val = randint(num1, num2)
+	return rand_val
+	
 
 # create function for difficulty
 # difficulty determines player's health level range
