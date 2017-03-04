c03a56a24e9910a2e88dd1a890b18afd94f4e168
unknown <sm.rambeau@gmail.com>
2017-03-03 17:00:15 -0800

    updating final project

--- final.py
+++ final.py
@@ -7,28 +7,59 @@ First, enter Name of their character
 Second, enter their character's gender
 Third, enter difficulty.  Accepted answers are Easy, Normal, and Hard
 """
+# accept user input from command line
+username = sys.argv[1].strip().capitalize()
+gender = sys.argv[2].strip().lower()
+difficulty = sys.argv[3].strip().lower()
+
 # create menu: start new game of quit
 def start_menu():
-	print("Welcome to the game!")
+	print("Welcome to the game, {}!".format(username))
 	print("Press 1 to start a new game")
 	print("Press 2 to quit")
 
 # display menu
 start_menu()
 
+menu_choice = 0
+
+#try:
+	#while menu_choice != 2:
+		#menu_choice = int(input("Please enter your selection: "))
+#except ValueError:
+	#print("That is not a valid selection.  Please try again")
+
 # create random number generator
 def random_number(num1, num2):
 	rand_val = randint(num1, num2)
 	return rand_val
 
-# accept user input from command line
-username = sys.argv[1].strip().capitalize()
-gender = sys.argv[2].strip().lower()
-difficulty = sys.argv[3].strip().lower()
+# function to set pronouns in the story based on user input
+def set_gameplay_pronoun(gender):
+	if gender == "female" or "woman" or "girl":
+		char_pronoun = "she"
+	elif gender == "male" or "man" or "boy":
+		char_pronoun = "he"
+	else:
+		char_pronoun = "they"
+	return char_pronoun
 
-print(username)
-print(gender)
-print(difficulty)
+# function to set possessive in the story based on user input
+def set_gameplay_possessive(gender):
+	if gender == "female" or "woman" or "girl":
+		char_possessive = "her"
+	elif gender == "male" or "man" or "boy":
+		char_possessive = "his"
+	else:
+		char_possessive = "their"
+	return char_possessive
+
+
+char_pronoun = set_gameplay_pronoun(gender)
+char_possessive = set_gameplay_possessive(gender)
+
+print("{} is playing the game".format(char_pronoun.capitalize()))
+print("It is {} game".format(char_possessive))
 
 # create function for difficulty
 # difficulty determines player's health level range
